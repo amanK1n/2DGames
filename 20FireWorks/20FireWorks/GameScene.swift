e3479a379c5b921111185fec91078ce623b8eadf
Sayed Aman Konen <sayed.konen@comviva.com>
2025-08-15 20:55:00 +0530

    Added firework game initial code

---
+++ 20FireWorks/20FireWorks/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  20FireWorks
+//
+//  Created by Sayed on 15/08/25.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+   
+}
